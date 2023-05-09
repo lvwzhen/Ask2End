9852c11c50a616902afbc7221f3120087e4f96b6
lvwzhen <lvwzhen@gmail.com>
2023-05-09 10:21:06 +0800

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,11 +6,9 @@ export default function Header() {
   return (
     <header className="md:flex text-center justify-between items-center w-full mt-5 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-3 mb-10 md:mb-0 justify-center">
-        <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
-          🧑🏼‍🏫 Ask2End{" "}
-          <small className=" font-medium text-base px-2 inline-block bg-fuchsia-500 text-white  rounded-full">
-            GPT-3.5-turbo
-          </small>
+        <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight flex items-center gap-2">
+          <img src="logo.png" alt="Ask2End logo" className="w-12 h-12" />
+          Ask2End
         </h1>
       </Link>
       <div>
@@ -36,4 +34,6 @@ export default function Header() {
     </header>
   );
 }
+
+
 
